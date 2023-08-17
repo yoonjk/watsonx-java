27cba408fac2e73e3371d213bc69c6129b20eea9
yoonjk <nexweb1@gmail.com>
2023-08-17 09:15:37 +0900

    first commit

---
+++ src/main/java/com/demo/watsonx/WatsonxJavaApplication.java
@@ -0,0 +1,13 @@
+package com.demo.watsonx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatsonxJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatsonxJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/watsonx/WatsonxJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.watsonx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatsonxJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
